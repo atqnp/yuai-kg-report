d5e909d82630d2e44f1eb5482bbbaa3f70435d53
atqnp <atiq_np@hotmail.com>
2019-03-06 16:21:21 +0900

    Update report.py

--- apps/report.py
+++ apps/report.py
@@ -15,7 +15,22 @@ layout = html.Div(
                 ], className="sheet padding-10mm"),
             html.Section([
                 html.H6("Academics Achievement - Teacher's Note"),
+                html.Div(id='display-notes'),
+                ], className="sheet padding-10mm"),
+            html.Section([
+                html.H6("Attendance"),
+                html.Div(id='display-attendance'),
+                html.Hr(),
                 html.Div(id='display-comments'),
+                html.Hr(),
+                html.Div(id='display-date'),
+                html.Br(),
+                html.Div(id='display-tname'),
+                html.Br(),
+                html.P('Headmistress : Yetti Dalimi'),
+                html.Br(),
+                html.P('Signature :'),
                 ], className="sheet padding-10mm"),
         ]
     )
+
